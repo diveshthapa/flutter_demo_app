db00451b6d071146d1b8dfa2bbb46b830a02dd98
Maya <dthapa007@gmail.com>
2022-03-13 11:23:41 +0545

    initaial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+Widget build(BuildContext context)
+
+return MaterialApp(
+home: Container(
+  child: text("testing First App"),
+),
+)}
